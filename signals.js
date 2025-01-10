274f1dbeb6a4202dc8a8ed314d4225e9e71230c2
Olsi Gjeci <olsi.developer@gmail.com>
2025-01-10 08:27:52 +0000

    starting state

---
+++ signals.js
@@ -0,0 +1,30 @@
+let subscriber = null;
+
+export function createSignal(value) {
+  const subscriptions = new Set();
+
+  const read = () => {
+    if (subscriber) {
+      subscriptions.add(subscriber);
+    }
+    return value;
+  };
+
+  const write = newValue => {
+    value = newValue;
+    subscriptions.forEach(fn => fn());
+  };
+  return [read, write];
+}
+
+export function createEffect(fn) {
+  subscriber = fn;
+  fn();
+  subscriber = null;
+}
+
+export function createMemo(fn) {
+  const [signal, setSignal] = createSignal();
+  createEffect(() => setSignal(fn()));
+  return signal;
+}
---
+++ workingSignals.js
@@ -0,0 +1,3 @@
+export function createSignal(value) {
+  console.log('createSignal');
+}
